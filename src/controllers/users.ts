97a0ba0a64aef0e4d5bf0fb1110389155622e6db
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-09 00:33:18 +0300

    Add updateUser controller and route

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-import { deleteUserById, getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
@@ -20,6 +20,31 @@ export const deleteUser = async (req: Request, res: Response) => {
     const deletedUser = await deleteUserById(id);
 
     return res.json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    // Check if username is provided
+    if (!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+
+    // Note: No need to check if user exists because isAuthenticated middleware already does that
+
+    // Update user's username
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
