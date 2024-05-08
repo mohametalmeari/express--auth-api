4beec4c5136e283df45ed2598d80eaeda42ef4af
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-08 22:42:19 +0300

    Add user login controller and route

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,52 @@ import { Request, Response } from "express";
 import { createUser, getUserByEmail } from "../db/users";
 import { authentication, random } from "../helpers";
 
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if required fields are present
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    // Check if user exists
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    // Check if password is correct
+    const expectedHash = authentication(user.authentication.salt, password);
+    if (user.authentication.password !== expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    // Create session token
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    // Send session token as cookie
+    res.cookie("AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
 export const register = async (req: Request, res: Response) => {
   try {
     const { email, password, username } = req.body;
--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 
-import { register } from "../controllers/authentication";
+import { login, register } from "../controllers/authentication";
 
 export default (router: Router) => {
   router.post("/auth/register", register); // User registration route is http://localhost:8080/auth/register
+  router.post("/auth/login", login); // User login route is http://localhost:8080/auth/login
 };
