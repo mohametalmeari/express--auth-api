41ac895d758e4046ad4ab37caec79a3bdc0bbbe4
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-09 01:45:49 +0300

    Implement login user after registration

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -66,10 +66,31 @@ export const register = async (req: Request, res: Response) => {
 
     // Create user
     const salt = random();
-    const user = await createUser({
+    await createUser({
       email,
       username,
       authentication: { salt, password: authentication(salt, password) },
+    });
+
+    // Login user after registration:
+    // - Get user after creation to get the _id
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    // - Create session token
+    const sessionSalt = random();
+    user.authentication.sessionToken = authentication(
+      sessionSalt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    // - Send session token as cookie
+    res.cookie("AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
     });
 
     return res.status(200).json(user).end();
